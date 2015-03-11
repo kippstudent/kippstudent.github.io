343cb77e0bdcdc413f541a289fc89a13a2531358
Stephanie Gonzalez <Sgonzalez@kippnyccp.org>
2015-03-11 09:32:32 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
+  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"];
 
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
-  }
+  };
   $('.card').click(function(){
     $(this).find('p').css('opacity', 1);
   });
