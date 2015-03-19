25945ac338d5ba398a904d6db6be81754543f8bb
Stephanie Gonzalez <Sgonzalez@kippnyccp.org>
2015-03-19 09:01:44 -0400

    Update application.js

--- shopping-list/application.js
+++ shopping-list/application.js
@@ -1,5 +1,8 @@
-$('#button').on('click',function(){
+$(document).ready(function()){
+ $('#button').on('click',function(){
   var item = $('#placeholder').val();
   $('ul').append('<li>'+item+'</li>');
+}); 
 });
+
   
