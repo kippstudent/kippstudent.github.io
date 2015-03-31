65503ead8dcf240fd97f75139cfd157a385abdf6
Stephanie Gonzalez <Sgonzalez@kippnyccp.org>
2015-03-31 08:34:10 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   //find a way for the program to choose a random number between 1 and 100, save this as a variable
-  
+  var comp_integer = Math.floor(Math.random()*100);
   
   //when the player clicks on the 'guess' button
   $('button').on('click', function(){
     //save their guess as a variable
-    
+    var play_guess = parseInt($('input').val());
     
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number...
