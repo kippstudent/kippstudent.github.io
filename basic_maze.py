f62ba10d0f93a583f5e13d4912d1af0dab0c146b
Stephanie Gonzalez <Sgonzalez@kippnyccp.org>
2015-05-20 08:53:51 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -1,11 +1,20 @@
-from gopigo import
+from gopigo import *
 import time
 import math
 set_speed(100)
-
-enc_tgt(1,1,18)
+set_left_speed(100)
+led_on()
+def move_forward(feet):
+  mm= feet * float (304.8)
+  steps = int (math.floor(mm / 11.34))
+enc_tgt(1,1,steps)
 time.sleep(.1)
 fwd()
-time.sleep(3)
+time.sleep(feet * 2)
 
-def move_forward(feet):
+def move_right():
+enc_tgt(1,0,14)
+time.sleep(.1)
+right
+()
+time.sleep(2)
