0957470e3e3a4b019f8515c0d9803f5293036c7a
Stephanie Gonzalez <Sgonzalez@kippnyccp.org>
2015-05-20 08:59:05 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -7,14 +7,25 @@ led_on()
 def move_forward(feet):
   mm= feet * float (304.8)
   steps = int (math.floor(mm / 11.34))
-enc_tgt(1,1,steps)
-time.sleep(.1)
-fwd()
-time.sleep(feet * 2)
+  enc_tgt(1,1,steps)
+  time.sleep(.1)
+  fwd()
+  time.sleep(feet * 2)
 
 def move_right():
-enc_tgt(1,0,14)
-time.sleep(.1)
-right
-()
-time.sleep(2)
+  enc_tgt(1,0,14)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+
+def move_left():
+  enc_tgt(0,1,14)
+  time.sleep(.1)
+  left()
+  time.sleep(2)
+  
+move_forward(4)
+move_right()
+move_foward(3)
+move_right()
+move_forward(4)
